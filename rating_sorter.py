10a84260b67b48cdf151cc7e8a8b4976f621b722
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-07-04 02:01:44 +0500

    Bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,12 @@
+import json
 
+with open("students.json", "r") as file:
+    students = json.load(file)
+
+students.sort(key=lambda x: x["score"])
+
+for i in range(len(students)):
+    students[i]["rank"] = i + 1
+
+with open("rating.json", "w") as file:
+    json.dump(students, file, indent=2)
