8943d31db5c534bfffb0b1e360da4f7261f3c60a
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-07-04 02:20:58 +0500

    Bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,12 +1,17 @@
 import json
 
+# 1. students.json faylini o‘qish
 with open("students.json", "r") as file:
     students = json.load(file)
 
-students.sort(key=lambda x: x["score"])
+# 2. Score bo‘yicha kamayish tartibida saralash
+students.sort(key=lambda x: x["score"], reverse=True)
 
+# 3. Rank (o‘rin) qo‘shish
 for i in range(len(students)):
-    students[i]["rank"] = i + 1
+    students[i]["rank"] = i + 1  # 1 dan boshlanadi
 
+# 4. rating.json fayliga yozish
 with open("rating.json", "w") as file:
     json.dump(students, file, indent=2)
+
